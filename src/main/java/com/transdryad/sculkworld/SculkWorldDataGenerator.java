9278bfee82c2a4d4d6edb548c3692d3a088ac7b6
transdryad <viswanathsam@gmail.com>
2023-09-02 15:01:48 -0600

    Base Config stuffs

--- src/main/java/com/transdryad/sculkworld/SculkWorldDataGenerator.java
+++ src/main/java/com/transdryad/sculkworld/SculkWorldDataGenerator.java
@@ -4,7 +4,7 @@ package com.transdryad.sculkworld;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
-public class ExampleModDataGenerator implements DataGeneratorEntrypoint {
+public class SculkWorldDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
 
