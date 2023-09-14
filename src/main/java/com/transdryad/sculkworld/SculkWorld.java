fa9fafca2bb6134c3fd2f3785ceb1e90ff11c6f8
transdryad <viswanathsam@gmail.com>
2023-09-14 16:33:51 -0600

    added first item. WOOOOO

--- src/main/java/com/transdryad/sculkworld/SculkWorld.java
+++ src/main/java/com/transdryad/sculkworld/SculkWorld.java
@@ -1,5 +1,6 @@
 package com.transdryad.sculkworld;
 
+import com.transdryad.sculkworld.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -11,6 +12,6 @@ public class SculkWorld implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/com/transdryad/sculkworld/item/ModItems.java
@@ -0,0 +1,28 @@
+package com.transdryad.sculkworld.item;
+
+import com.transdryad.sculkworld.SculkWorld;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    public static final Item SHARD = registerItem("shard", new Item(new FabricItemSettings()));
+
+    private static void addItemsToIngredientItemGroup(FabricItemGroupEntries entries) {
+        entries.add(SHARD);
+    }
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, new Identifier(SculkWorld.MOD_ID, name), item);
+    }
+    public static void registerModItems() {
+        SculkWorld.LOGGER.info("Registering Items for" + SculkWorld.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::addItemsToIngredientItemGroup);
+    }
+}
