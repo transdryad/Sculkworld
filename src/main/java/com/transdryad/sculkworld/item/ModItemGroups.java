b36ddabf22528accaaf6399fe7bb9912c2d4053d
Hazel <109479539+transdryad@users.noreply.github.com>
2023-09-18 12:26:51 -0600

    Dev (#4)
    * Working on startup instructions
    
    * Base Config stuffs
    
    * Base Config stuffs
    
    * b (#3)
    
    * Base Config and prep for work (#1)
    
    * Working on startup instructions
    
    * Base Config stuffs
    
    * Base Config stuffs
    
    * Create FUNDING.yml (#2)
    
    * added first item. WOOOOO
    
    * Second Item and item group?

---
+++ src/main/java/com/transdryad/sculkworld/item/ModItemGroups.java
@@ -0,0 +1,24 @@
+package com.transdryad.sculkworld.item;
+
+import com.transdryad.sculkworld.SculkWorld;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+public class ModItemGroups {
+    public static final ItemGroup SCULK_GROUP = Registry.register(Registries.ITEM_GROUP, new Identifier(SculkWorld.MOD_ID,
+            "sculk"),
+            FabricItemGroup.builder().displayName(Text.translatable("itemgroup.sculk"))
+                    .icon(() -> new ItemStack(ModItems.INGOT)).entries((displayContext, entries) -> {
+                        entries.add(ModItems.SHARD);
+                        entries.add(ModItems.INGOT);
+                    }).build());
+
+    public static void registerItemGroups() {
+        SculkWorld.LOGGER.info("Registering Item Groups for " + SculkWorld.MOD_ID);
+    }
+}
